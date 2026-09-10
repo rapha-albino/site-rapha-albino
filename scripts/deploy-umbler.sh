912dcc691f4a3e68f4d20c136f24124a22903027
Raphael Donaire Albino <raphael.albino@gmail.com>
2026-09-10 09:55:34 -0300

    Respect Umbler file permissions during static publishing

--- scripts/deploy-umbler.sh
+++ scripts/deploy-umbler.sh
@@ -10,7 +10,7 @@ KEY="${UMBLER_SSH_KEY:-$HOME/.ssh/id_ed25519_umbler_rapha_albino}"
 TARGET="${UMBLER_TARGET:-public/}"
 
 npm run build
-rsync -azO --delete-delay \
+rsync -azO --no-perms --delete-delay \
   --exclude="temporary.html" \
   --exclude="wp-content/" \
   --exclude="wp-includes/" \
